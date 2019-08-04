66c085bd3318f1f57ddce0def00efc497498e034
shoya <swgt1500@gmail.com>
2019-08-04 16:28:57 +0900

    SQL生成クラスを作成 グループ画面のサービスクラスを作成

---
+++ src/jp/co/salon/service/GroupService.java
@@ -0,0 +1,14 @@
+package jp.co.salon.service;
+
+import java.sql.Connection;
+
+public class GroupService extends WebApiBase {
+
+	/** コンストラクタ */
+	public GroupService() {}
+	public GroupService(Connection con) {
+		super(con);
+	}
+
+
+}
---
+++ src/jp/co/salon/service/sql/CreateGroupSQL.java
@@ -0,0 +1,79 @@
+package jp.co.salon.service.sql;
+
+public class CreateGroupSQL {
+
+	/**
+	 * 概要：ユーザIDに紐づくグループ情報を<br>
+	 * すべて取得するSQLを作成する
+	 * @return sql SQL文
+	 */
+	public static String getGroups() {
+		StringBuilder sql = new StringBuilder();
+
+		sql.append("SELECT ");
+		sql.append("	group_id, ");
+		sql.append("	group_name, ");
+		sql.append("	modified ");
+		sql.append("FROM ");
+		sql.append("	public.trn_chat_group AS groups ");
+		sql.append("WHERE ");
+		sql.append("	groups.user_id = ?;");
+
+		return sql.toString();
+	}
+
+	/**
+	 * 概要：ユーザIDに紐づくグループ情報を<br>
+	 * １件取得するSQLを作成する
+	 * @return sql SQL文
+	 */
+	public static String getGroup() {
+		StringBuilder sql = new StringBuilder();
+
+		sql.append("SELECT ");
+		sql.append("	group_id, ");
+		sql.append("	group_name, ");
+		sql.append("	modified ");
+		sql.append("FROM ");
+		sql.append("	public.trn_chat_group AS groups ");
+		sql.append("WHERE ");
+		sql.append("	groups.group_id = ?;");
+
+		return sql.toString();
+	}
+	/**
+	 * 概要：ユーザIDに紐づくグループ名を<br>
+	 * 更新するSQLを作成する
+	 * @return sql SQL文
+	 */
+	public static String insertGroup() {
+		StringBuilder sql = new StringBuilder();
+
+		sql.append("INSERT INTO ");
+		sql.append("	public.trn_chat_group ( ");
+		sql.append("	 	group_id, ");
+		sql.append("	 	user_id, ");
+		sql.append("	 	group_name) ");
+		sql.append("VALUES ( ?, ?, ?);");
+
+		return sql.toString();
+	}
+
+	/**
+	 * 概要：ユーザIDに紐づくグループ名を<br>
+	 * 更新するSQLを作成する
+	 * @return sql SQL文
+	 */
+	public static String updateGroupName() {
+		StringBuilder sql = new StringBuilder();
+
+		sql.append("UPDATE ");
+		sql.append("	public.public.trn_chat_group AS groups ");
+		sql.append("SET ");
+		sql.append("	groups.group_name = ? ");
+		sql.append("WHERE ");
+		sql.append("	groups.group_id = ?;");
+
+		return sql.toString();
+	}
+}
